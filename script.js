04185c2f29463d61898e706c4fd4ae99d64696f8
sankeerthr10 <sankeerthr10@gmail.com>
2019-06-21 12:06:26 +0530

    new changes

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function onSignIn(googleUser){
 	
 	var profile = googleUser.getBasicProfile();
+	alert("signed in to function");
 	$(".g-signin2").css("display","none");
 	$("#welcome").text("Welcome" + profile.getName());
 	$(".programData").css("display","block");
